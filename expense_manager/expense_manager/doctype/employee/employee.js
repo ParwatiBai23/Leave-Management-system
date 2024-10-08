7119d41b04b673ac64c27df5da96cfad434d935a
Parwati Bai <parwatibai23@navgurukul.org>
2024-10-08 12:47:27 +0530

    first commit

---
+++ expense_manager/expense_manager/doctype/employee/employee.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Parwati Bai and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Employee", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ expense_manager/expense_manager/doctype/expense_type/test_expense_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Parwati Bai and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestExpenseType(FrappeTestCase):
+	pass
